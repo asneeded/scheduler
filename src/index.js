6772c58d4c0ea8874d9036ec1b115e5a1f0e0094
adam sneed <adamdsneed@gmail.com>
2018-08-22 21:48:19 -0600

    Testing webpack

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import './style.css';
+import printMe from './print.js';
+
+
+
+function component() {
+    let element = document.createElement('div');
+    let btn = document.createElement('button');
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+
+
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
+}
+
+document.body.appendChild(component());
